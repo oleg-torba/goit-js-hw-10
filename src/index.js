9e489c75cb745eb2326aaa9141b20409d6256eba
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2023-02-22 00:12:50 +0200

    some

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const searchBtn = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
  const countryInfo = document.querySelector('.country-info');
 searchBtn.addEventListener('input', onInputSearch);
-
+let inputValue = ""
 // function onInputSearch(e) {
 //   const inputValue = e.target.value.trim();
 // fetchCountries()
@@ -41,28 +41,30 @@ function fetchCountries(name) {
   fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`)
     .then(response => response.json())
     .then(onSuccess)
+    .catch()
     
 }
 
 function onSuccess(country){
    
-       const countries = Object.values(country).forEach(value=> 
+       Object.values(country).forEach(value=> 
         markup(value.name, value.capital, value.population, value.flags))
        
+        
 
-
-}
-
-function onInputSearch(e) {
-  const inputValue = e.target.value;
-  fetchCountries(inputValue);
 }
 function markup (name, capital, population, flags){
-    // const markupList = `
+    if(inputValue === ""){
+        countryList.innerHTML = "";
+    countryInfo.innerHTML = ""
+    return
+}
     
     countryList.innerHTML = `
     <h1>country: ${name}</h1>
     `;
+   
+  
 
     countryInfo.innerHTML = `
     <p>capital: ${capital}</p>
@@ -71,3 +73,10 @@ function markup (name, capital, population, flags){
     
     
 }
+
+function onInputSearch(e) {
+inputValue = e.target.value.trim();
+  fetchCountries(inputValue);
+
+}
+
